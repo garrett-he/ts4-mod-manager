f3ec149399b6d1660d4a5c6a2df713805e53fcbd
Garrett HE <garrett.he@outlook.com>
2023-11-11 11:18:31 +0800

    Add dependencies for ant-design-vue

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/reset.css";
+
 createApp(App)
     .use(router)
     .use(store)
+    .use(Antd)
     .mount("#app");
