62c53e44d2f7ce5ae693d5dcb4e1c719265bf7dd
Garrett HE <garrett.he@outlook.com>
2023-11-12 12:00:53 +0800

    Add information for mods

--- electron/ts4mm.ts
+++ electron/ts4mm.ts
@@ -1,11 +1,14 @@
-import fs from "node:fs";
+import fs from "fs-extra";
 import path from "node:path";
-import {Mod} from "ts4mm/types";
+import {Mod, ModInfo} from "ts4mm/types";
 
 function loadMod(dir: string): Mod {
+    const infoFile = path.resolve(dir, "info.json");
+
     return <Mod>{
         id: path.basename(dir),
-        dir
+        dir,
+        info: fs.existsSync(infoFile) ? <ModInfo>fs.readJSONSync(infoFile) : undefined
     };
 }
 
--- src/types.d.ts
+++ src/types.d.ts
@@ -6,5 +6,16 @@ declare module "ts4mm/types" {
     interface Mod {
         id: string;
         dir: string;
+        info?: ModInfo | undefined;
+    }
+
+    interface ModInfo {
+        name: string;
+        category: string;
+        tags: string[];
+        author?: string | undefined;
+        version?: string | undefined;
+        url?: string | undefined;
+        description: string;
     }
 }
