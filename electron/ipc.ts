b90a5ed488c311861a244b47308572e4e4842565
Garrett HE <garrett.he@outlook.com>
2023-11-16 10:10:50 +0800

    Add API functions

--- electron/ipc.ts
+++ electron/ipc.ts
@@ -1,8 +1,56 @@
-import {ipcMain} from "electron";
+import path from "node:path";
+import fs from "fs-extra";
+import {ipcMain, dialog, shell, OpenDialogSyncOptions} from "electron";
 import {Mod} from "ts4mm/types";
-import {discoverMods} from "./ts4mm";
+import MarkdownIt from "markdown-it";
+import {discoverMods, installMod, uninstallMod, importMod} from "./ts4mm";
 import config from "./config";
 
 ipcMain.handle("discoverMods", async (): Promise<Mod[]> => {
     return discoverMods(config["library"]);
+});
+
+
+ipcMain.handle("installMod", async (_, mod: Mod) => {
+    installMod(mod, config["target"]);
+});
+
+ipcMain.handle("uninstallMod", async (_, mod: Mod) => {
+    uninstallMod(mod);
+});
+
+ipcMain.handle("importMods", async (): Promise<Mod[]> => {
+    const filePaths = dialog.showOpenDialogSync(<OpenDialogSyncOptions>{
+        filters: [
+            {name: "Mod Packages", extensions: ["zip", "tar.gz", "tgz"]},
+            {name: "All Files", extensions: ["*"]}
+        ],
+        properties: ["openFile", "multiSelections"]
+    });
+
+    if (!filePaths) {
+        return [];
+    }
+
+    return await filePaths.map(filePath => importMod(config["library"], filePath));
+});
+
+ipcMain.handle("browseMod", async (event, mod: Mod): Promise<string> => {
+    return shell.openPath(mod.dir);
+});
+
+ipcMain.handle("getModReadMe", async (event, mod: Mod): Promise<string> => {
+    const readmeFile = path.resolve(mod.dir, "README.md");
+
+    if (!readmeFile) {
+        return "N/A";
+    }
+
+    return (new MarkdownIt({
+        html: true,
+        linkify: true,
+        typographer: true
+    })).render(fs.readFileSync(readmeFile).toString()).replace(/<img src="(.+)">/g, (match, $1) => {
+        return `<img src="${path.resolve(mod.dir, $1)}" alt="">`;
+    });
 });
--- electron/preload.ts
+++ electron/preload.ts
@@ -3,4 +3,10 @@ import {Mod} from "ts4mm/types";
 
 contextBridge.exposeInMainWorld("$api", {
     discoverMods: async (): Promise<Mod[]> => ipcRenderer.invoke("discoverMods"),
+    installMod: (mod: Mod): Promise<void> => ipcRenderer.invoke("installMod", mod),
+    uninstallMod: (mod: Mod): Promise<void> => ipcRenderer.invoke("uninstallMod", mod),
+    importMods: (): Promise<Mod[]> => ipcRenderer.invoke("importMods"),
+    deleteMod: (mod: Mod): Promise<void> => ipcRenderer.invoke("deleteMod", mod),
+    browseMod: (mod: Mod): Promise<void> => ipcRenderer.invoke("browseMod", mod),
+    getModReadMe: (mod: Mod): Promise<string> => ipcRenderer.invoke("getModReadMe", mod)
 });
